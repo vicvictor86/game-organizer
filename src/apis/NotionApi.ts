15f6656f6b0d9b40abdc4a20c2f31cfb4bcd7346
Victor Emmanuel <vicbem@hotmail.com>
2023-02-21 22:17:22 -0300

    Insert new game informations in table

---
+++ src/apis/NotionApi.ts
@@ -0,0 +1,131 @@
+import { Client } from "@notionhq/client";
+import { AppError } from "../errors/AppError";
+import { TimesToBeat } from "../interfaces/GameInfo";
+import { SelectOptions } from '../interfaces/SelectOptions';
+
+interface PlatformOptions {
+  id: string;
+
+  name: string;
+}
+
+interface PlatformOptionsResponse {
+  platformOptions: PlatformOptions[];
+
+  undefinedPlatform: PlatformOptions;
+}
+
+interface Request {
+  platformNames: string[];
+  releaseDate: string;
+  timeToBeat: TimesToBeat;
+}
+
+const notion = new Client({
+  auth: process.env.NOTION_KEY,
+})
+
+const databaseGameID = process.env.NOTION_GAME_DATABASE_ID;
+const databasePlatformID = process.env.NOTION_PLATFORM_DATABASE_ID;
+
+export async function readItem() {
+  console.log(databaseGameID)
+  if (databaseGameID) {
+    await notion.databases.query({
+      database_id: databaseGameID,
+    }).then(response => {
+      console.log(response.results[2].properties.platform)
+    })
+  }
+}
+
+export async function getPlatformsOptions(): Promise<PlatformOptionsResponse> {
+  if (!databasePlatformID) {
+    console.log('Error: No database ID');
+    throw new AppError('Error: No database ID');
+  };
+
+  const queryAllPlatforms = await notion.databases.query({
+    database_id: databasePlatformID,
+  });
+
+  const entries = queryAllPlatforms.results;
+
+  const platformsIdsWithName = entries.map(entry => {
+    return {
+      id: entry.id,
+      name: entry.properties.name.title[0].text.content as string,
+    } as PlatformOptions;
+  });
+
+  const undefinedPlatform = platformsIdsWithName.find(platform => platform.name === 'Undefined Platform');
+
+  if(!undefinedPlatform) {
+    throw new AppError('Error: No undefined platform, please create one');
+  }
+
+  return { platformOptions: platformsIdsWithName, undefinedPlatform };
+}
+
+export async function insertGame(gameName: string, gameInfo: Request): Promise<void> {
+  if (!databaseGameID || !databasePlatformID) {
+    console.log('Error: No database ID');
+    return;
+  };
+
+  const gameDatabaseInfo = await notion.databases.retrieve({
+    database_id: databaseGameID,
+  });
+
+  const statusOptions = gameDatabaseInfo.properties.status.select.options as SelectOptions[];
+  const { platformOptions, undefinedPlatform } = await getPlatformsOptions();
+
+  if (!platformOptions) {
+    throw new AppError('Error: No platform options');
+  };
+
+  const platformId = platformOptions.find(platformOption => gameInfo.platformNames.some(platformName => platformName === platformOption.name))?.id;
+
+  const statusId = statusOptions.find(status => status.name === 'Want to Play')?.id || statusOptions[0].id
+
+  const response = await notion.pages.create({
+    parent: {
+      database_id: databaseGameID,
+    },
+    properties: {
+      title: {
+        title: [
+          {
+            type: 'text',
+            text: {
+              content: gameName,
+            },
+          },
+        ],
+      },
+      platform: {
+        relation: [
+          {
+            id: platformId || undefinedPlatform.id,
+          },
+        ],
+      },
+      status: {
+        select: {
+          id: statusId,
+        },
+      },
+      time_to_beat: {
+        number: gameInfo.timeToBeat.main,
+      },
+      release_date: {
+        date: {
+          start: gameInfo.releaseDate,
+        },
+      },
+    },
+  });
+
+  console.log(response);
+  console.log("Game inserted with success!")
+}
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,11 @@
 import 'dotenv/config';
 import apicalypse, { ApicalypseConfig } from "apicalypse";
-import { HowLongToBeatService, HowLongToBeatEntry } from 'howlongtobeat'
-
 import APIResponse from './interfaces/APIResponse';
 import GameInfo from './interfaces/GameInfo';
-import { SelectOptions } from './interfaces/SelectOptions';
 
 import getToken from './auth/getToken';
-
-import { Client } from "@notionhq/client";
-
-const hltbService = new HowLongToBeatService();
-const notion = new Client({
-  auth: process.env.NOTION_KEY,
-})
-const databaseGameID = process.env.NOTION_GAME_DATABASE_ID;
-const databasePlatformID = process.env.NOTION_PLATFORM_DATABASE_ID;
+import { insertGame } from './apis/NotionApi';
+import { getGameTimeToBeat } from './apis/HLTBApi';
 
 async function getGameInfo(gameName: string, requestOptions: ApicalypseConfig): Promise<APIResponse[]> {
   const response = apicalypse(requestOptions);
@@ -43,20 +33,18 @@ async function getInfosByID(data: APIResponse[], requestOptions: ApicalypseConfi
 
   const gameInfoPromises = data.map(async game => {
     const unixTimeStampToMillis = new Date(game.first_release_date * 1000);
-    const hltbResponse = await hltbService.search(game.name);
-    const firstGameHltb = hltbResponse.at(0);
+    const timesToBeat = await getGameTimeToBeat(game.name);
 
     const actualGameInfo = {
       name: game.name,
-      console: gamesGenresAndPlatform.find(gameResolve => gameResolve.gameId === game.id)?.promisesPlatform.data,
+      platform: gamesGenresAndPlatform.find(gameResolve => gameResolve.gameId === game.id)?.promisesPlatform.data,
       genres: gamesGenresAndPlatform.find(gameResolve => gameResolve.gameId === game.id)?.promisesGenre.data,
       rating: game.total_rating,
       releaseDate: unixTimeStampToMillis,
-      difficulty: 0,
       timeToBeat: {
-        main: firstGameHltb?.gameplayMain || 0,
-        MainExtra: firstGameHltb?.gameplayMainExtra || 0,
-        Completionist: firstGameHltb?.gameplayCompletionist || 0,
+        main: timesToBeat?.main || 0,
+        MainExtra: timesToBeat?.MainExtra || 0,
+        Completionist: timesToBeat?.Completionist || 0,
       },
     } as GameInfo;
 
@@ -68,128 +56,31 @@ async function getInfosByID(data: APIResponse[], requestOptions: ApicalypseConfi
   return gamesInfo;
 }
 
-async function insertNewGameToExcel(gameName: string, requestOptions: ApicalypseConfig) {
+async function insertNewGameInTable(gameName: string, requestOptions: ApicalypseConfig) {
   const gamesInfo = await getGameInfo(gameName, requestOptions);
 
   const gameInformationComplete = await getInfosByID(gamesInfo, requestOptions);
 
-
-
-  // gamesInfoInCsv.forEach(async gameInfo => {
-
-  // });
-}
-
-async function readItem() {
-  console.log(databaseGameID)
-  if (databaseGameID) {
-    await notion.databases.query({
-      database_id: databaseGameID,
-    }).then(response => {
-      console.log(response.results[2].properties.platform)
-    })
-  }
-}
-
-async function getPlatformsOptions(){
-  if(!databasePlatformID) {
-    console.log('Error: No database ID');
-    return;
-  };
-
-  const queryAllPlatforms = await notion.databases.query({
-    database_id: databasePlatformID,
+  gameInformationComplete.forEach(async gameInfo => {
+    const platformNames = gameInfo.platform?.map(platform => platform.name);
+    const releaseDate = gameInfo.releaseDate.toISOString();
+    const timeToBeat = gameInfo.timeToBeat;
+    await insertGame(gameInfo.name, { platformNames, releaseDate, timeToBeat });
   });
-
-  const entries = queryAllPlatforms.results;
-
-  const platformsIdsWithName = entries.map(entry => {
-    return {
-      id: entry.id,
-      name: entry.properties.name.title[0].text.content as string,
-    }
-  });
-
-  return platformsIdsWithName;
 }
-
-async function insertGame(gameName: string, platformName: string) {
-  if (!databaseGameID || !databasePlatformID) {
-    console.log('Error: No database ID');
-    return;
-  };
-
-  const gameDatabaseInfo = await notion.databases.retrieve({
-    database_id: databaseGameID,
-  });
-
-  const statusOptions = gameDatabaseInfo.properties.status.select.options as SelectOptions[];
-  const platformOptions = await getPlatformsOptions();
-  
-  if(!platformOptions) {
-    console.log('Error: No platform options');
-    return;
-  };
-
-  const platformId = platformOptions.find(platformOption => platformOption.name === platformName)?.id;
-  const statusId = statusOptions.find(status => status.name === 'Want to Play')?.id || statusOptions[0].id
-
-  if(!platformId) {
-    console.log('Error: No platform ID');
-    return;
-  };
-
-  const response = await notion.pages.create({
-    parent: {
-      database_id: databaseGameID,
-    },
-    properties: {
-      title: {
-        title: [
-          {
-            type: 'text',
-            text: {
-              content: gameName,
-            },
-          },
-        ],
-      },
-      platform: {
-        relation: [
-          {
-            id: platformId,
-          },
-        ],
-      },
-      status: {
-        select: {
-          id: statusId,
-        },
-      },
-      release_date: {
-        date: {
-          start: '2021-01-01',
-        },
-      },
-    },
-  });
-
-  console.log(response);
-}
-
 getToken().then(async responseToken => {
-  // const { access_token, expiresIn, tokenType } = responseToken;
+  const { access_token, expiresIn, tokenType } = responseToken;
 
-  // const requestOptions: ApicalypseConfig = {
-  //   baseURL: process.env.API_BASE_URL,
-  //   method: 'POST',
-  //   headers: {
-  //     'Accept': 'application/json',
-  //     'Client-ID': process.env.CLIENT_ID,
-  //     'Authorization': `Bearer ${access_token}`
-  //   },
-  // };
+  const requestOptions: ApicalypseConfig = {
+    baseURL: process.env.API_BASE_URL,
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Client-ID': process.env.CLIENT_ID,
+      'Authorization': `Bearer ${access_token}`
+    },
+  };
 
   // await readItem();
-  await insertGame('sonic', "PlayStation 4");
+  await insertNewGameInTable('zelda', requestOptions)
 });
