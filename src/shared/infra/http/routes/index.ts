3e14ce5175baeba2047df36c07a08ed5c3a77849
Victor Emmanuel <vicbem@hotmail.com>
2023-04-13 23:47:43 -0300

    Integration route add

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import gamesRouter from '../../../../modules/games/http/routes/games.routes';
+import integrationRouter from '../../../../modules/integration/http/routes/integration.routes';
 
 const routes = Router();
 
 routes.use('/games', gamesRouter);
+routes.use('/integration', integrationRouter);
 
 export default routes;
