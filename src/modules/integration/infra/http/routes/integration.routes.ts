d0efc18725d2bcc6b82f6671a88a61b2519bff5c
Victor Emmanuel <vicbem@hotmail.com>
2023-05-18 14:01:17 -0300

    Fixing /info router function

--- src/modules/integration/infra/http/routes/integration.routes.ts
+++ src/modules/integration/infra/http/routes/integration.routes.ts
@@ -6,6 +6,6 @@ const integrationRouter = Router();
 const integrationController = new IntegrationController();
 
 integrationRouter.get('/', ensureAuthenticate, integrationController.create);
-integrationRouter.get('/info', ensureAuthenticate, integrationController.create);
+integrationRouter.get('/info', ensureAuthenticate, integrationController.index);
 
 export default integrationRouter;
